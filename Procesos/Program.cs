b5315a5b1987e79f2b9e2cdc65c5bbe1a4aca329
Dennis Quiguire <instagram.24@outlook.com>
2021-07-26 08:00:48 -0500

    Final Proyect

--- Procesos/Program.cs
+++ Procesos/Program.cs
@@ -6,7 +6,34 @@ namespace Procesos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Console.WriteLine("*******************************");
+            //using (var db = new SchoolContext())
+            //{
+            //    var blogs = db.Clientes
+            //        .Where(b => b.clienteId > 3)
+            //        .OrderBy(b => b.generoId)
+            //        .ToList();
+
+            //    foreach (var venta in blogs)
+            //    {
+            //        Console.WriteLine(
+            //        String.Format("ClienteId: {0} \n" +
+            //              "Nombre: {1} \n" +
+            //              "Edad: {2} Su id de genero es : {3} \n",
+            //    //"GeneroId: {3} \n",
+
+            //    venta.clienteId,
+            //    venta.nombre,
+            //    venta.edad,
+            //    venta.generoId
+            //            //venta.generoId
+
+            //            )
+            //        );
+            //    }
+            //}
+            
         }
     }
 }
